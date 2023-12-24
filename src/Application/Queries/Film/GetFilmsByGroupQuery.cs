5739a510e43cb63cf84da9c240a79037dc7e7834
annv <annv@nobisoft.com.vn>
2023-12-24 09:14:55 +0700

    1

---
+++ src/Application/Queries/Film/GetFilmsByGroupQuery.cs
@@ -0,0 +1,11 @@
+using Application.DataTransferObjects.Film.Requests;
+using Application.DataTransferObjects.Film.Responses;
+using Domain.Common.Pagination.OffsetBased;
+using MediatR;
+
+namespace Application.Queries.Film;
+
+public class GetListFilmsByGroupQuery : IRequest<OffsetPaginationResponse<FilmResponse>>
+{
+    public required ViewListFilmsByGroupRequest Request { get; set; }
+}
